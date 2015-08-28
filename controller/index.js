68340540b81248dbd6560eda23ecd498a98c8a21
Kosuke Nagano <gm.charlie@gmail.com>
2015-08-29 01:21:58 +0900

    Externalized video list

--- controller/index.js
+++ controller/index.js
@@ -1,10 +1,8 @@
 var videoList = {}
-videoList[0]={videoId: '-rgEZe18Dv8', videoTitle:'ももいろクローバーZ - なんてこったパンナコッタ.(full ver,)'};
-videoList[1]={videoId: '0I8-yo5MPQM', videoTitle:'Who is Momoiro Clover Z?'};
-
+videoList = require('../data/videoList.json');
 var pageTitle = 'momocall'
 
 exports.index = function (req, res) {
-  res.render('index', { title: pageTitle, list: videoList });
+  res.render('index', { title: pageTitle, list: videoList.list });
   console.log("index");
 };
