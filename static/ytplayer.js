35af97b3bae856deb350325c91619309b4d832c1
Kosuke Nagano <gm.charlie@gmail.com>
2015-08-28 17:35:12 +0900

    unite ytplayer.js and client.js

--- static/ytplayer.js
+++ static/ytplayer.js
@@ -10,6 +10,22 @@ function setVideoId(id) {
   console.log('setVideoId:' + id + ':' + videoId);
 }
 
+// WebSocket Connection
+var socket = io.connect('http://localhost:8888');
+
+socket.on('connect', function() {
+  console.log('connect');
+});
+
+socket.on('message', function(msg) {
+  console.log(msg);
+  $('#entrypoint').append($( msg.msg ));
+});
+
+socket.on('disconnect', function(){
+  console.log('disconnect');
+});
+
 // Replace the 'ytplayer' element with an <iframe> and
 // YouTube player after the API code downloads.
 var player;
@@ -42,15 +58,20 @@ function onPlayerStateChange(event) {
   }
   else if(state == YT.PlayerState.PLAYING) {
     console.log('playing');
+    socket.emit('start');
   }
   else if(state == YT.PlayerState.PAUSED) {
-    console.log('paused:' + player.getCurrentTime() ); 
+    currentTime = player.getCurrentTime();
+    console.log('paused:' + currentTime);
+    socket.emit('pause', {currentTime : currentTime });
   }
   else if(state == YT.PlayerState.BUFFERING) {
-    console.log('buffering'); 
+    console.log('buffering');
+    socket.emit('buffering');
   }
   else if(state == YT.PlayerState.CUED) {
     console.log('cued'); 
   }
 }
+
 
